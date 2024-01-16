cd523bbff02358b42106b71cc35fd8a22fa2d1e3
luong tran <luongluong99783@gmail.com>
2024-01-16 12:17:16 +0700

    notification

--- src/Context/ChatProvider.jsx
+++ src/Context/ChatProvider.jsx
@@ -4,6 +4,7 @@ import { localService } from '~/config/localService'
 // eslint-disable-next-line react-refresh/only-export-components
 export const chatContext = createContext({})
 export const ChatProvider = ({ children }) => {
+  const [notification, setNotification] =useState([])
   const [user, setUser] = useState({})
   const [selectedChat, setSelectedChat] = useState()
   const [chat, setChat] = useState([])
@@ -11,7 +12,7 @@ export const ChatProvider = ({ children }) => {
     setUser (localService.getItem('USER_INFO'))
   }, [])
   return (
-    <chatContext.Provider value={{ user, setUser, selectedChat, setSelectedChat, chat, setChat } }>
+    <chatContext.Provider value={{ user, setUser, selectedChat, setSelectedChat, chat, setChat, notification, setNotification } }>
       {children}
     </chatContext.Provider>
   )
